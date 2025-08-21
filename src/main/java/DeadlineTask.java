b7d9ce281c4bcc444a678ffc8d9011dd73c3b044
Chee Hin <tancheeehin@gmail.com>
2025-08-22 03:12:32 +0800

    Misc file modifications for DeadlineTask
    - unknown modifications that were missed from the last commit unsure why

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+public class DeadlineTask extends Task {
+    private String deadline;
 
-public class DeadlineTask {
+    public DeadlineTask(String description, String deadline) {
+        super(description);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return "[D] " + super.toString() + "(by: " + this.deadline + ")";
+    }
 }
