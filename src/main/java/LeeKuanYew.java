6e8116cef67aa64c6f26027f37dda74280d9dd9f
Chee Hin <tancheeehin@gmail.com>
2025-08-21 23:12:40 +0800

    Update chatbot identity to Lee Kuan Yew
    - Changed name to Lee Kuan Yew
    - Edited intro message to fit new persona
    - Removed all references of Duke

---
+++ src/main/java/LeeKuanYew.java
@@ -0,0 +1,10 @@
+public class LeeKuanYew {
+    public static void main(String[] args) {
+        System.out.println(
+                """
+                        Hello! I'm Lee Kuan Yew, founding father of Singapore.
+                        How can I help my fellow countryman today?
+                        """
+        );
+    }
+}
