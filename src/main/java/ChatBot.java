8956d0e7456ea1be2ea765d3159d80b15216c626
Chee Hin <tancheeehin@gmail.com>
2025-08-22 01:20:45 +0800

    Implement response logic for task marking
    - Users can now mark and unmark tasks
    - Custom messages added for all scenarios, including exceptions

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,10 +1,12 @@
+import com.sun.source.util.TaskListener;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class ChatBot {
 
-    List<String> tasks;
+    List<Task> tasks;
 
     public ChatBot() {
         this.tasks = new ArrayList<>();
@@ -18,12 +20,21 @@ public class ChatBot {
     }
 
     public String getResponse(String input) {
-        switch (input.toLowerCase()) {
+        String[] parts = input.split(" ", 2);
+        String command = parts[0].toLowerCase();
+        switch (command) {
             case "list":
                 return getTaskList();
 
+            case "mark":
+                return markTask(parts, true);
+
+            case "unmark":
+                return markTask(parts, false);
+
             default:
-                tasks.add(input);
+                Task task = new Task(input);
+                tasks.add(task);
                 return "I shall add: " + input;
         }
     }
@@ -38,9 +49,41 @@ public class ChatBot {
 
         StringBuilder sb = new StringBuilder("Your work awaits you!\n");
         for (int i = 0; i < tasks.size(); i++) {
-            sb.append((i + 1) + ". " + tasks.get(i) + "\n");
+            Task task = tasks.get(i);
+            char isDone = task.checkDone() ? 'X' : ' ';
+            sb.append((i + 1) + task.toString() + "\n");
         }
         return sb.toString().trim();
+    }
+
+    public String markTask(String[] parts, boolean mark) {
+        // check if there is anything after mark
+        if (parts.length < 2) return "You must specify a task number!";
+
+        try {
+            int taskNum = Integer.parseInt(parts[1]) - 1;
+
+            if (taskNum < 0) {
+                return "No negative numbers! Don't be stupid.";
+            }
+
+            if (taskNum >= tasks.size()) {
+                return "You overestimate your workload. Get to work!";
+            }
+
+            Task task = tasks.get(taskNum);
+            if (mark) {
+                task.markDone();
+                return "You have the IRON in you! Good job!\n" + task;
+
+            } else {
+                task.unmarkDone();
+                return "No shame in failure. Pick yourself up, Singapore needs you.\n" + task;
+            }
+        } catch (NumberFormatException e) {
+            return "That's not a valid number.";
+
+        }
     }
 
     public String getEndMessage() {
