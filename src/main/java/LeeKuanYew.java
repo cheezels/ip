a3d9daab3364b1420a11e16d2c6e48b4e377ceb3
Chee Hin <tancheeehin@gmail.com>
2025-08-22 00:03:17 +0800

    Allow user input and ultilise ChatBot class
    - main program uses ChatBot class now instead of handling logic on its
      own

--- src/main/java/LeeKuanYew.java
+++ src/main/java/LeeKuanYew.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class LeeKuanYew {
     public static void main(String[] args) {
-        System.out.println(
-                """
-                        Hello! I'm Lee Kuan Yew, founding father of Singapore.
-                        How can I help my fellow countryman today?
-                        """
-        );
+        ChatBot chatBot = new ChatBot("Lee Kuan Yew");
+        System.out.println(chatBot.getIntroMessage());
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String userInput = sc.nextLine();
+
+            // Quit condition
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println(chatBot.getEndMessage());
+                break;
+            }
+
+            System.out.println(chatBot.getResponse(userInput));
+        }
+        sc.close();
+
     }
 }
