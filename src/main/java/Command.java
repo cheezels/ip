1a0dc06bacc6b5399f2c18693cfde0db39fe63f6
Chee Hin <tancheeehin@gmail.com>
2025-08-28 22:42:42 +0800

    fix: Adjust parameters for execute method

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,5 +1,5 @@
 public abstract class Command {
-    public abstract void execute();
+    public abstract void execute(TaskList taskList, Ui ui, Storage storage);
 
     public boolean checkExit() {
         return false;
