c807c60e07efea10d32a59b6c43e9386e3c791b1
Chee Hin <tancheeehin@gmail.com>
2025-08-28 22:30:43 +0800

    refractor: Task list now implemented with TaskList class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void add(Task task) {
+        this.tasks.add(task);
+    }
+
+    public Task get(int index) {
+        return this.tasks.get(index);
+    }
+
+    public int size() {
+        return this.tasks.size();
+    }
+
+}
