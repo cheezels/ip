fce8dae53ca491f4a240f93b688818e269ca0f64
Chee Hin <tancheeehin@gmail.com>
2025-09-01 17:44:33 +0800

    feat: Add find command to chatbot
    Basic logic and functionality added. Some edge cases and bugs have to be
    addressed.

--- src/main/java/LeeKuanYew/ChatBot.java
+++ src/main/java/LeeKuanYew/ChatBot.java
@@ -6,6 +6,7 @@ import LeeKuanYew.Command.ListCommand;
 import LeeKuanYew.Command.MarkCommand;
 import LeeKuanYew.Command.DeleteCommand;
 import LeeKuanYew.Command.SaveCommand;
+import LeeKuanYew.Command.FindCommand;
 import LeeKuanYew.Command.ToDoCommand;
 import LeeKuanYew.Command.DeadlineCommand;
 import LeeKuanYew.Command.EventCommand;
@@ -17,68 +18,71 @@ public class ChatBot {
         String command = parts[0].toLowerCase();
 
         switch (command) {
-            case "bye":
-                return new ByeCommand();
+        case "bye":
+            return new ByeCommand();
 
-            case "list":
-                return new ListCommand();
+        case "list":
+            return new ListCommand();
 
-            case "mark":
-                if (parts.length < 2) {
-                    throw new Exception("You must specify a task number!");
-                }
+        case "mark":
+            if (parts.length < 2) {
+                throw new Exception("You must specify a task number!");
+            }
 
-                try {
-                    int i = Integer.parseInt(parts[1]) - 1;
-                    return new MarkCommand(i, true);
-                } catch (NumberFormatException e) {
-                    throw new Exception("This is not a number.");
-                }
+            try {
+                int i = Integer.parseInt(parts[1]) - 1;
+                return new MarkCommand(i, true);
+            } catch (NumberFormatException e) {
+                throw new Exception("This is not a number.");
+            }
 
-            case "unmark":
-                if (parts.length < 2) {
-                    throw new Exception("You must specify a task number!");
-                }
+        case "unmark":
+            if (parts.length < 2) {
+                throw new Exception("You must specify a task number!");
+            }
 
-                try {
-                    int j = Integer.parseInt(parts[1]) - 1;
-                    return new MarkCommand(j, false);
-                } catch (NumberFormatException e) {
-                    throw new Exception("This is not a number.");
-                }
+            try {
+                int j = Integer.parseInt(parts[1]) - 1;
+                return new MarkCommand(j, false);
+            } catch (NumberFormatException e) {
+                throw new Exception("This is not a number.");
+            }
 
-            case "delete":
-                if (parts.length < 2) {
-                    throw new Exception("You must specify a task number!");
-                }
+        case "delete":
+            if (parts.length < 2) {
+                throw new Exception("You must specify a task number!");
+            }
 
-                try {
-                    int k = Integer.parseInt(parts[1]) - 1;
-                    return new DeleteCommand(k);
-                } catch (NumberFormatException e) {
-                    throw new Exception("This is not a number.");
-                }
+            try {
+                int k = Integer.parseInt(parts[1]) - 1;
+                return new DeleteCommand(k);
+            } catch (NumberFormatException e) {
+                throw new Exception("This is not a number.");
+            }
 
-            case "save":
-                return new SaveCommand();
+        case "save":
+            return new SaveCommand();
 
-            case "todo":
-                return new ToDoCommand(parts[1]);
+        case "find":
+            return new FindCommand(parts[1]);
 
-            case "deadline":
-                String[] deadlineParts = parts[1].split("/", 2);
-                if (deadlineParts.length != 2) throw new Exception("Discipline requires deadlines.");
+        case "todo":
+            return new ToDoCommand(parts[1]);
 
-                return new DeadlineCommand(deadlineParts[0], deadlineParts[1]);
+        case "deadline":
+            String[] deadlineParts = parts[1].split("/", 2);
+            if (deadlineParts.length != 2) throw new Exception("Discipline requires deadlines.");
 
-            case "event":
-                String[] eventParts = parts[1].split("/", 3);
-                if (eventParts.length != 3) throw new Exception("It takes time to build a country like Singapore.");
+            return new DeadlineCommand(deadlineParts[0], deadlineParts[1]);
 
-                return new EventCommand(eventParts[0], eventParts[1], eventParts[2]);
+        case "event":
+            String[] eventParts = parts[1].split("/", 3);
+            if (eventParts.length != 3) throw new Exception("It takes time to build a country like Singapore.");
 
-            default:
-                throw new Exception("YOUR WORDS MEAN NOTHING!");
+            return new EventCommand(eventParts[0], eventParts[1], eventParts[2]);
+
+        default:
+            throw new Exception("YOUR WORDS MEAN NOTHING!");
         }
 
     }
